cab0547acad91e039d5d1025f6824a5fb25412b5
Frank <fr.heggeness@gmail.com>
2019-04-12 15:18:32 -1000

    added more functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
 'use strict';
-
+// var moment = require('moment');
+// console.log(moment('20111031', 'YYYYMMDD').fromNow());
 // create sticky header
 
 window.onscroll = function() {
   stickyFunc();
 };
 
-let header = document.getElementById('top-header');
+let header = document.getElementById('headerBox');
 let sticky = header.offsetTop;
 
 function stickyFunc() {
@@ -28,25 +29,82 @@ function sendRequest(subreddit) {
   };
 }
 
-header.addEventListener('click', sendRequest('surfing'));
+// get data/create posts function below
 
 function getData() {
   let responseData = JSON.parse(this.responseText);
   let responseChildren = responseData.data.children;
+  postsBox.innerHTML = '';
   console.log(responseChildren);
   for (let i = 0; i < responseChildren.length; i++) {
     // create new postBox
     let newPost = document.createElement('div');
     newPost.className = 'postBox';
-    newPost.innerHTML = responseChildren[i].data.title;
     postsBox.appendChild(newPost);
+    let innerPostBox = document.createElement('div');
+    innerPostBox.className = 'innerPostBox';
+    newPost.appendChild(innerPostBox);
 
     // add image
     if (responseChildren[i].data.thumbnail !== 'self') {
       let postImage = document.createElement('img');
       postImage.className = 'postImages';
       postImage.src = responseChildren[i].data.thumbnail;
-      newPost.appendChild(postImage);
+      innerPostBox.appendChild(postImage);
     }
+
+    // add title
+    let postTitle = document.createElement('div');
+    postTitle.className = 'postTitles';
+    postTitle.innerHTML = responseChildren[i].data.title;
+    innerPostBox.appendChild(postTitle);
+
+    // add author/date/upvotes container
+    let statsBox = document.createElement('div');
+    statsBox.className = 'postStats';
+    // grab author,date, upvotes
+    let author = responseChildren[i].data.author;
+    // let date = moment(responseChildren[i].data.created, 'YYYYMMDD');
+    // console.log(date);
+    let upvotes = responseChildren[i].data.score;
+    statsBox.innerHTML = `By : ${author} ●  DATE HERE  ● Upvotes: ${upvotes}`;
+
+    // add post content
+    let postContent = document.createElement('div');
+    postContent.className = 'postContent';
+    let fullParagraph = responseChildren[i].data.selftext;
+    if (fullParagraph !== '') {
+      var reducedParagraph = fullParagraph.substr(0, 150);
+      postContent.innerHTML = reducedParagraph + '...';
+    }
+    innerPostBox.appendChild(statsBox);
+    innerPostBox.appendChild(postContent);
   }
 }
+
+// add click event to nav bar
+
+surfNav.addEventListener('click', sendRequest('surfing'));
+spaceNav.addEventListener('click', sendRequest('astrophotography'));
+hawaiiNav.addEventListener('click', sendRequest('hawaii'));
+randomNav.addEventListener('click', sendRequest(getSub()));
+
+// random subreddit generator below
+
+function randomSub() {
+  let randomSubArray = ['pic', 'EarthPorn', 'FoodPorn', 'askReddit', 'dogswithjobs', 'nba', 'javascript'];
+  let randomNum = Math.floor(Math.random() * 7);
+  return randomSubArray[randomNum];
+}
+function getSub() {
+  return randomSub();
+}
+
+// endless scroll attempt
+window.onscroll = function(ev) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    loadMore();
+  }
+};
+
+function loadMore() {}
